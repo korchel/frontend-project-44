e6458662a704c24f32c0803c554aceafab2d4065
Elena Korchuganova <korchel21@gmail.com>
2022-10-02 12:54:21 +0300

    name question edited

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,5 +2,6 @@
 import { questionName } from '../src/cli.js'
 
 console.log('Welcome to the Brain Games!');
+console.log('May I have your name?');
 const usersName = questionName();
 console.log(`Hello, ${usersName}!`);
